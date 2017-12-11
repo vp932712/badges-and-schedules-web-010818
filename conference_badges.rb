6205573adda17ad1a6330066c335866efb6bc025
Vaibhav Patel <vp932712@gmail.com>
2017-12-11 23:37:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  empty = []
+  array.each do |people|
+    empty.push(badge_maker(people))
+  end
+  empty
+end
